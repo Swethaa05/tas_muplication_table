ccd9641fe0bfa4465f1b3cf462b19cfc18906966
Swethaa05 <113167807+Swethaa05@users.noreply.github.com>
2023-07-25 17:48:03 +0530

    Add files via upload

---
+++ multiplication_table.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class multiplication_table {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("ENTER NUMBER:");
+        int n =s.nextInt();
+        for (int i = 1;i <= 10; i++) {
+            System.out.println(n+"*"+i+"="+n*i);
+        }
+    }
+
+}
